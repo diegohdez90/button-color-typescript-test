e9c450f118ef8d4167e21485c075e2c5d859bbd9
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2022-06-01 09:53:06 -0500

    test: add fire event

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
+const setup = () => render(<App />);
 
-
-test('button has correct initial color', () => {
-  render(<App />);
+test('button has correct initial styles', () => {
+  setup();
   const button = screen.getByRole('button', {
     name: 'Change to blue'
   });
@@ -15,10 +15,15 @@ test('button has correct initial color', () => {
   });
 });
 
-test('button has correct initial text', () => {
-  
-});
-
 test('button turns blue when is clicked', () => {
+  setup();
+  const button = screen.getByRole('button', {
+    name: 'Change to blue'
+  });
 
+  fireEvent.click(button);
+  expect(button).toHaveStyle({
+    backgroundColor: 'blue'
+  });
+  expect(button).toHaveTextContent('Change to red');
 });
